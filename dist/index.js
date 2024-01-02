6ba0d3e2217be64e493d74bbffb0d8cf3d334821
kodeninj <femifadiya.segun@gmail.com>
2024-01-02 22:35:18 +0100

    initialize Typscript repo

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let num = 4;
+let numb = "hello";
+if (num > 3) {
+    numb = "hello greater ones";
+}
+console.log(numb);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let num: number = 4;
+let numb: string = "hello";
+if (num > 3) {
+	numb = "hello greater ones";
+}
+console.log(numb);
